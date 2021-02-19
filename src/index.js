fbb04003a5bbda1ae3ab1847b5e90a6915f313e5
Oscar <voscarmv@gmail.com>
2021-02-19 12:44:22 -0600

    Fixed index bug.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import store from './store/index';
-import addArticle from './actions/index';
 import App from './components/App';
-
-window.store = store;
-window.addArticle = addArticle;
 
 render(
   <Provider store={store}>
