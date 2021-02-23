d90394a60e6d28c058408e7e887eddc0b06ec834
Oscar <voscarmv@gmail.com>
2021-02-22 20:00:03 -0600

    Filter updater form prototype complete.

--- src/components/BreedFilter.js
+++ src/components/BreedFilter.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateFilter } from '../actions/index';
 
 const filterForm = () => {
   const dispatch = useDispatch();
   const [filter, setFilter] = useState(
-    {
-      property: '',
-      value: '',
+    {},
+  );
+  useEffect(
+    () => {
+      dispatch(updateFilter(filter));
     },
   );
   const handleChange = e => {
+    let value = 0;
+    let property = '';
+    if (e.target.type === 'checkbox') {
+      property = e.target.value;
+      value = e.target.checked ? 1 : 0;
+    } else {
+      property = e.target.id;
+      value = e.target.value;
+    }
     setFilter(
       {
-        property: e.target.value,
-        value: e.target.checked,
+        property,
+        value,
       },
     );
   };
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch(updateFilter(filter));
-  };
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <label htmlFor="experimental">
         Experimental
         <input onChange={handleChange} type="checkbox" id="experimental" value="experimental" />
       </label>
-      <button type="submit">Submit</button>
+      <label htmlFor="intelligence">
+        Intelligence
+        <input onChange={handleChange} type="number" id="intelligence" min="1" max="5" />
+      </label>
     </form>
   );
 };
