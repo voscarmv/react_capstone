0e264d8116c16735aa616f8750168b05be5227c8
Oscar <voscarmv@gmail.com>
2021-02-26 17:01:14 -0600

    Basic style layout designed.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,36 @@
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Button, Collapse } from 'react-bootstrap';
+import { useState } from 'react';
 import BreedFilter from './BreedFilter';
 import BreedMenu from './BreedMenu';
 
-const Nav = () => (
-  <>
-    <Navbar>
-      <BreedMenu />
-    </Navbar>
-    <BreedFilter />
-  </>
-);
-
+const Nav = () => {
+  const [open, setOpen] = useState(
+    false,
+  );
+  return (
+    <>
+      <Navbar>
+        <Navbar.Brand>
+          CATalog
+        </Navbar.Brand>
+        <Button
+          onClick={() => { setOpen(!open); }}
+          aria-controls="collapse-filter-form"
+          aria-expanded={open}
+        >
+          Filter
+        </Button>
+        <div className="p-4">
+          Show breed:
+        </div>
+        <BreedMenu />
+      </Navbar>
+      <Collapse in={open}>
+        <div id="collapse-filter-form">
+          <BreedFilter />
+        </div>
+      </Collapse>
+    </>
+  );
+};
 export default Nav;
