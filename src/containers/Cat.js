85f240e036616e65c198862dc752f0fa185c9691
Oscar <voscarmv@gmail.com>
2021-03-08 21:22:33 -0600

    Destructure cat component

--- src/containers/Cat.js
+++ src/containers/Cat.js
@@ -5,13 +5,15 @@ import { useSelector } from 'react-redux';
 const Cat = () => {
   const cat = useSelector(state => state.catState);
   if (cat.data[0]) {
+    const catData = cat.data[0].breeds[0];
+    const { description, cfa_url: cfaUrl, wikipedia_url: wikipediaUrl } = catData;
     return (
       <Container className="m-3">
         <Row>
           <Col>
-            <div>{cat.data[0].breeds[0].description}</div>
-            <div><a href={cat.data[0].breeds[0].cfa_url}>CFA website</a></div>
-            <div><a href={cat.data[0].breeds[0].wikipedia_url}>Wikipedia page</a></div>
+            <div>{description}</div>
+            <div><a href={cfaUrl}>CFA website</a></div>
+            <div><a href={wikipediaUrl}>Wikipedia page</a></div>
           </Col>
           <Col>
             <div>
