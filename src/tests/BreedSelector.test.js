f32f1b574a1a7ec056559c683b07516d90ba8e36
Oscar <voscarmv@gmail.com>
2021-03-10 14:50:22 -0600

    Added Snapshot test for breedSelector

--- src/tests/BreedSelector.test.js
+++ src/tests/BreedSelector.test.js
@@ -150,4 +150,7 @@ const BreedSelectorTest = TestRenderer.create(
   <BreedSelector breedsData={breeds} handleSelectChange={() => true} />,
 );
 
-console.log(BreedSelectorTest.toJSON());
+it('Breed Selector renders correctly', () => {
+  const tree = BreedSelectorTest.toJSON();
+  expect(tree).toMatchSnapshot();
+});
