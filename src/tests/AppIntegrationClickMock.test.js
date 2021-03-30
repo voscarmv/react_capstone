433954f167fd707e6515caa370952f1f5b70d3cf
Oscar <voscarmv@gmail.com>
2021-03-29 20:42:26 -0600

    Created deterministic mock tests

---
+++ src/tests/AppIntegrationClickMock.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  render, screen, waitFor, fireEvent,
+} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../store/index';
+import App from '../containers/App';
+import 'regenerator-runtime/runtime';
+import * as actions from '../actions/index';
+import breeds from './BreedsResponse';
+import cat from './CatResponse';
+
+jest.mock('../actions/index');
+
+describe('App', () => {
+  beforeEach(() => {
+    actions.fetchBreeds.mockImplementation(
+      () => ({ type: 'FETCH_BREEDS_SUCCESS', payload: breeds }),
+    );
+    actions.fetchCat.mockImplementation(
+      () => ({ type: 'FETCH_CAT_SUCCESS', payload: cat }),
+    );
+  });
+  afterEach(() => {
+    actions.fetchBreeds.mockRestore();
+    actions.fetchCat.mockRestore();
+  });
+  test('renders Cat component', async () => {
+    const fullApp = render(<Provider store={store}><App /></Provider>);
+    const { getByTestId, asFragment } = fullApp;
+    await waitFor(
+      () => {
+        expect(screen.getByText('Siberian'));
+      },
+    );
+    fireEvent.click(getByTestId('aege'));
+    await waitFor(
+      () => {
+        expect(screen.getByText('Wikipedia page'));
+      },
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
